44e938cc1f716cbf8321ffdbe29aecfeaec18450
莱德斯诺 <Q663397592@outlook.com>
2026-08-08 11:55:53 +0800

    Update siteConfig.ts

--- XHBlogs/siteConfig.ts
+++ XHBlogs/siteConfig.ts
@@ -5,11 +5,11 @@ export const siteConfig = {
   title: "个人博客",
   faviconUrl: "/69c1e38ac1846.jpg",
   authorName: "谢子杰",
-  bio: "热爱苹果生态与前沿应用开发的普通人，致力打造兼具性能与优秀体验的软件工具",
+  bio: "在代码、学术与分子动力学模拟间穿梭的普通人。近期正埋头于 GROMACS 模拟研究与神经网络计算。",
 
   navTitle: "谢子杰",
 
-  // 👇 导航栏中间的那个后缀/分隔符（默认是 の）
+  // 👇 【新增】导航栏中间的那个后缀/分隔符（默认是 の）
   navSuffix: "の",
 
   navAfter: "宝藏之地",
@@ -18,8 +18,10 @@ export const siteConfig = {
   avatarUrl: "/69c1e38ac1846.jpg",
 
   // 3. 网站背景设置 (二选一)
+  // 如果想用纯图片背景，请在下面 bgImage 写路径，并将 useGradient 设为 false
   useGradient: false,
   themeColors: ["#a18cd1", "#fbc2eb", "#a1c4fd", "#c2e9fb"], // 呼吸流动的颜色组合
+// 修改这里：变成图片数组
   bgImages: ["https://bu.dusays.com/2026/03/24/69c1e38b4c370.jpg", "https://bu.dusays.com/2026/03/24/69c26fe4acdb5.jpg", "https://bu.dusays.com/2026/03/24/69c26fe4d9486.jpg"],
 
   // 4. 文章默认封面图 (当 Markdown 没写 cover 时显示)
@@ -31,7 +33,7 @@ export const siteConfig = {
   social: {
     github: "wxidqlvk",
     gitee: "wxidqlvk",
-    google: "ficocamell650@gmail.com",
+    google: "ficocamell650@gamil.com",
     email: "q663397592@icloud.com",
     qq: "663397592",
     wechat: "WXIDQLVK",
@@ -42,19 +44,16 @@ export const siteConfig = {
   chatterTitle: "云端杂谈", // 你可以改成任何你喜欢的名字
   chatterDescription: "代码、学术、提瓦特与泰拉大陆的碎片记录",
 
-  // 👇 全局背景弹幕配置
+
+  // 👇 【新增】：全局背景弹幕配置
   danmakuList: ["在干嘛呢？", "有笨蛋嘛？", "前方高能反应！", "GROMACS 跑起来了吗？", "MD 模拟什么时候才能出图啊", "Graph Neural Networks 炼丹中...", "BUG 修复进度 99%", "今天背单词了吗？", "Tailwind CSS 拯救前端", "写算法中", "睡大觉中", "到底在干嘛？"],
-  
   gitalkConfig: {
     clientID: "Ov23lilsJzCV5sDiSfQg",
     clientSecret: "757f846357f94454029d2b2bca5b9aee24f1e236",
     repo: "wxidqlvk.github.io",
     owner: "wxidqlvk",
     admin: ["wxidqlvk"],
-    id: "about", // 👈 强制锁死用这个 id 作为唯一标识，彻底解决 405
-    distractionFreeMode: false, // 👈 防止全屏模式冲突
   },
-
   buildDate: "2026-03-23T00:00:00", // 建站日期
   footerBadges: [{"name": "Next.js 15", "color": "text-sky-500", "svg": "<path d=\"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8 z\"/>"}, {"name": "React 19", "color": "text-cyan-400", "svg": "<path d=\"M12 22.6l-9.8-5.6V5.6L12 0l9.8 5.6v11.4l-9.8 5.6zm-8.2-6.5l8.2 4.7 8.2-4.7V7.5L12 2.8 3.8 7.5v8.6z\"/>"}, {"name": "Tailwind 4", "color": "text-teal-400", "svg": "<path d=\"M12.001,4.8c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624C13.666,10.618,15.027,12,18.001,12 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C16.337,6.182,14.976,4.8,12.001,4.8z M6.001,12c-3.2,0-5.2,1.6-6,4.8c1.2-1.6,2.6-2.2,4.2-1.8c0.913,0.228,1.565,0.89,2.288,1.624c1.177,1.194,2.538,2.576,5.512,2.576 c3.2,0,5.2-1.6,6-4.8c-1.2,1.6-2.6,2.2-4.2,1.8c-0.913-0.228-1.565-0.89-2.288-1.624C10.337,13.382,8.976,12,6.001,12z\"/>"}],
   icpConfig: {
